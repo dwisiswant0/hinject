809ace56c3b08ab91aaf44f99fb74e30c6ca18d2
dw1 <dwi.siswanto98@gmail.com>
2020-07-02 01:01:42 +0700

    :sparkles: Add verbose mode. Closes #2

--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"bufio"
 	"crypto/tls"
+	"flag"
 	"fmt"
-	"github.com/logrusorgru/aurora"
 	"io/ioutil"
 	"net"
 	"net/http"
@@ -12,14 +12,20 @@ import (
 	"strings"
 	"sync"
 	"time"
+
+	"github.com/logrusorgru/aurora"
 )
 
 func main() {
 
 	client := newClient()
-	var wg sync.WaitGroup
 	sc := bufio.NewScanner(os.Stdin)
+	var wg sync.WaitGroup
 	var forwarded = "0a6d8cfc90fb8ef81240cd1f127409098dd846e1.local"
+	var verboseMode bool
+	flag.BoolVar(&verboseMode, "v", false, "be verbose")
+
+	flag.Parse()
 
 	for sc.Scan() {
 		rawURL := sc.Text()
@@ -30,19 +36,25 @@ func main() {
 
 			req, err := http.NewRequest("GET", rawURL, nil)
 			req.Header.Set("X-Forwarded-Host", forwarded)
-			if err != nil {
+			if err != nil && verboseMode {
+				fmt.Printf("[  %s  ] %s\n", aurora.Red("FAILED").String(), rawURL)
 				return
 			}
 
 			resp, err := client.Do(req)
-			if err != nil {
+			if err != nil && verboseMode {
+				fmt.Printf("[  %s  ] %s\n", aurora.Red("FAILED").String(), rawURL)
 				return
 			}
 			defer resp.Body.Close()
 
 			data, _ := ioutil.ReadAll(resp.Body)
 			if strings.Contains(string(data), forwarded) {
-				fmt.Printf("[%s] %s\n", aurora.Green("VULN").String(), rawURL)
+				fmt.Printf("[%s] %s\n", aurora.Green("VULNERABLE").String(), rawURL)
+			} else {
+				if verboseMode {
+					fmt.Printf("[ %s ] %s\n", aurora.Yellow("NOT VULN").String(), rawURL)
+				}
 			}
 		}()
 	}
